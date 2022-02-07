5d3323c6e4812994e58717f77dd42450089956b6
Tommy Taraldsvik <epleregime@gmail.com>
2022-02-07 13:12:01 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,24 @@
 input.onButtonPressed(Button.A, function () {
-    tilbakeMeldingsAlternativene.push("positive")
+    feedbackAlternatives.push("positive")
     basic.showString("A")
     basic.pause(200)
     basic.clearScreen()
 })
 input.onButtonPressed(Button.AB, function () {
-    tilbakeMeldingsAlternativene.push("neutral")
+    feedbackAlternatives.push("neutral")
     basic.showString("C")
     basic.pause(500)
     basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function () {
-    tilbakeMeldingsAlternativene.push("negative")
+    feedbackAlternatives.push("negative")
     basic.showString("B")
     basic.pause(200)
     basic.clearScreen()
 })
 input.onGesture(Gesture.Shake, function () {
     OLED.clear()
-    tilfeldigValg = tilbakeMeldingsAlternativene._pickRandom()
+    tilfeldigValg = feedbackAlternatives._pickRandom()
     if (tilfeldigValg == "neutral") {
         tilfeldigTall = randint(0, neutrals.length - 1)
         OLED.writeStringNewLine(neutrals[tilfeldigTall])
@@ -29,9 +29,9 @@ input.onGesture(Gesture.Shake, function () {
         tilfeldigTall = randint(0, positives.length - 1)
         OLED.writeStringNewLine(positives[tilfeldigTall])
     }
-    if (tilbakeMeldingsAlternativene.length >= 4) {
-        tilbakeMeldingsAlternativene.pop()
-        basic.showString("" + (tilbakeMeldingsAlternativene.length))
+    if (feedbackAlternatives.length >= 4) {
+        feedbackAlternatives.pop()
+        basic.showString("" + (feedbackAlternatives.length))
         basic.pause(100)
         basic.clearScreen()
     }
@@ -40,14 +40,14 @@ function setLists () {
     neutrals = ["Det kan skje", "Vet ikke", "Muligens"]
     positives = ["Absolutt", "Ja", "Helt sikkert"]
     negatives = ["Nei", "Aldri", "Beklager"]
-    tilbakeMeldingsAlternativene = ["positive", "neutral", "negative"]
+    feedbackAlternatives = ["positive", "neutral", "negative"]
 }
 let positives: string[] = []
 let negatives: string[] = []
 let neutrals: string[] = []
 let tilfeldigTall = 0
 let tilfeldigValg = ""
-let tilbakeMeldingsAlternativene: string[] = []
+let feedbackAlternatives: string[] = []
 setLists()
 let barHeight = 0
 OLED.init(128, 64)
